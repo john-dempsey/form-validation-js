5150feae0e556ad44e7782914c2bd2c9a5396381
John Dempsey <dempseyj@iadt.ie>
2023-02-09 12:59:40 +0000

    Form validation with Validator class #2

--- form.js
+++ form.js
@@ -2,34 +2,19 @@ class Validator {
     constructor() {}
 
     validate(form) {
+        let errors = {};
+
         let nameField = form.querySelector("#name");
         let categoryInput = form.querySelector("#category");
         let experienceBtns = form.querySelectorAll("[name=experience]");
         let languageBtns = form.querySelectorAll("[name='languages[]']");
         
-        let nameError = form.querySelector("#name_error");
-        let categoryError = form.querySelector("#category_error");
-        let experienceError = form.querySelector("#experience_error");
-        let languagesError = form.querySelector("#languages_error");
-        
-        let errorExists = false;
-        
-        function showError(errorField, errorMessage) {
-            errorField.innerHTML = errorMessage;
-            errorExists = true;
-        }
-        nameError.innerHTML = "";
-        categoryError.innerHTML = "";
-        experienceError.innerHTML = "";
-        languagesError.innerHTML = "";
-        errorExists = false;
-    
         if (nameField.value === "") {
-            showError(nameError, "Name is required");
+            errors["name"] = "Name is required";
         }
     
         if (categoryInput.value === "") {
-            showError(categoryError, "Category is required");
+            errors["category"] = "Category is required";
         }
     
         let expSelected = false;
@@ -41,7 +26,7 @@ class Validator {
             }
         }
         if (!expSelected) {
-            showError(experienceError, "Experience is required");
+            errors["experience"] = "Experience is required";
         }
     
         let minLang = 1;
@@ -54,23 +39,66 @@ class Validator {
             }
         }
         if (countLang < minLang || countLang > maxLang) {
-            showError(languagesError, "Choose one or two languages");
+            errors["languages"] = "Choose one or two languages";
         }
 
-        return errorExists;
+        return errors;
     }
 }
+
+function showErrors(errors, form) {
+    if ("name" in errors) {
+        let nameError = form.querySelector("#name_error");
+        nameError.innerHTML = errors["name"];
+    }
+    if ("category" in errors) {
+        let categoryError = form.querySelector("#category_error");
+        categoryError.innerHTML = errors["category"];
+    }
+    if ("experience" in errors) {
+        let experienceError = form.querySelector("#experience_error");
+        experienceError.innerHTML = errors["experience"];
+    }
+    if ("languages" in errors) {
+        let languagesError = form.querySelector("#languages_error");
+        languagesError.innerHTML = errors["languages"];
+    }
+
+    // for (let key in errors) {
+    //     let spanElement = form.querySelector("#" + key + "_error");
+    //     let errorMessage = errors[key];
+    //     spanElement.innerHTML = errorMessage;
+    // }
+}
+
+function clearErrors(form) {
+    let nameError = form.querySelector("#name_error");
+    let categoryError = form.querySelector("#category_error");
+    let experienceError = form.querySelector("#experience_error");
+    let languagesError = form.querySelector("#languages_error");
+    
+    nameError.innerHTML = "";
+    categoryError.innerHTML = "";
+    experienceError.innerHTML = "";
+    languagesError.innerHTML = "";
+}
+
+
 
 function onClick(e) {
     e.preventDefault();
 
     let commentForm = document.getElementById("comment_form");
+    clearErrors(commentForm);
     let validator = new Validator();
-    let errorExists = validator.validate(commentForm);
+    let errors = validator.validate(commentForm);
 
     // if there was no errors then submit the form
-    if (!errorExists) {
+    if (Object.keys(errors).length === 0) {
         commentForm.submit();
+    }
+    else {
+        showErrors(errors, commentForm);
     }
 }
 
